021743ba8b76a2b6705dda1db515300350bc5684
David Odewusi <davidodewusi@gmail.com>
2022-07-07 12:40:48 +0100

    Implementing the checkbox

---
+++ js/script.js
@@ -0,0 +1,5 @@
+"use strict";
+const addTodo = document.querySelector(".add-todo-checkbox");
+const checkbox = document.querySelectorAll(".todo-checkbox");
+const todoText = document.querySelectorAll(".todo-check-text");
+const delTodo = document.querySelectorAll(".todo-delete");
